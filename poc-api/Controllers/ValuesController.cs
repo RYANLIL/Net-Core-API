ce97fbe734823a51627581a32c7e6439182eda90
Unknown <ryan.lildhar@gmail.com>
2018-01-24 22:38:09 -0500

    Added some tests for IAction and ModelState checking

--- poc-api/Controllers/ValuesController.cs
+++ poc-api/Controllers/ValuesController.cs
@@ -1,38 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace poc_api.Controllers
 {
+
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class ValuesController : ControllerBase
     {
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "ChangedVal", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id, string q)
         {
-            return "value";
+            return Ok(new Value { Id = id, Text = "Value" + id });
         }
 
         // POST api/values
+        // formbody handles format baseed on what format handlers available from controller base
+        //when using types can check if val conforms to model Value
+        // IAction takes care of setting status 200 400 500 etc
         [HttpPost]
+        public IActionResult Post([FromBody]Value val)
         {
             if(!ModelState.IsValid)
             {
-                throw new InvalidOperationException("Invalid Operations");
+                return BadRequest(ModelState);
+                //throw new InvalidOperationException("Invalid Operations");
+
             }
+
+            //save data to db
+
+            return CreatedAtAction("Get", new { id = val.Id }, val);
         }
 
-        // PUT api/values/5
+        // PUT api/values/5        
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
